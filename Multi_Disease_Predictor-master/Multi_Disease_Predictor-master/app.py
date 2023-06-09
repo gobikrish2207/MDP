9b347b2a5fd6caaf28a9bde227f87890b674d5d1
Gobi Krishnan <gobikrish03@gmail.com>
2023-06-09 13:47:02 +0530

    MDP

---
+++ Multi_Disease_Predictor-master/Multi_Disease_Predictor-master/app.py
@@ -0,0 +1,212 @@
+import joblib
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+import pandas as pd
+import pickle
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import pickle
+import numpy as np
+from PIL import Image
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import tensorflow as tf
+import tensorflow.keras.backend as K
+import tensorflow.keras.applications.vgg16 as vgg16
+import tensorflow.keras.applications.vgg19 as vgg19
+import tensorflow.keras.applications.resnet50 as resnet50
+import tensorflow.keras.applications.xception as xception
+import tensorflow.keras.applications.inception_v3 as inception_v3
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+
+
+
+def predict(values, dic):
+    if len(values) == 8:
+        model = pickle.load(open('models/diabetes.pkl','rb'))
+        values = np.asarray(values)
+        return model.predict(values.reshape(1, -1))[0]
+    elif len(values) == 26:
+        model = pickle.load(open('models/breast_cancer.pkl','rb'))
+        values = np.asarray(values)
+        return model.predict(values.reshape(1, -1))[0]
+    elif len(values) == 13:
+        model = pickle.load(open('models/heart.pkl','rb'))
+        values = np.asarray(values)
+        return model.predict(values.reshape(1, -1))[0]
+    elif len(values) == 18:
+        model = pickle.load(open('models/kidney.pkl','rb'))
+        values = np.asarray(values)
+        return model.predict(values.reshape(1, -1))[0]
+    elif len(values) == 10:
+        model = pickle.load(open('models/liver.pkl','rb'))
+        values = np.asarray(values)
+        return model.predict(values.reshape(1, -1))[0]
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/help')
+def help():
+    return render_template("help.html")
+
+
+@app.route('/terms')
+def terms():
+    return render_template("tc.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('home'))
+
+        return render_template("login.html", form=form)
+    return render_template("login.html", form=form)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect("/login")
+    return render_template('signup.html', form=form)
+
+
+
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/diabetes", methods=['GET', 'POST'])
+def diabetesPage():
+    return render_template('diabetes.html')
+
+@app.route("/cancer", methods=['GET', 'POST'])
+def cancerPage():
+    return render_template('breast_cancer.html')
+
+@app.route("/heart", methods=['GET', 'POST'])
+def heartPage():
+    return render_template('heart.html')
+
+@app.route("/kidney", methods=['GET', 'POST'])
+def kidneyPage():
+    return render_template('kidney.html')
+
+@app.route("/liver", methods=['GET', 'POST'])
+def liverPage():
+    return render_template('liver.html')
+
+@app.route("/malaria", methods=['GET', 'POST'])
+def malariaPage():
+    return render_template('malaria.html')
+
+@app.route("/pneumonia", methods=['GET', 'POST'])
+def pneumoniaPage():
+    return render_template('pneumonia.html')
+
+@app.route("/predict", methods = ['POST', 'GET'])
+def predictPage():
+    try:
+        if request.method == 'POST':
+            to_predict_dict = request.form.to_dict()
+            to_predict_list = list(map(float, list(to_predict_dict.values())))
+            pred = predict(to_predict_list, to_predict_dict)
+    except:
+        message = "Please enter valid Data"
+        return render_template("home.html", message = message)
+
+    return render_template('predict.html', pred = pred)
+
+@app.route("/malariapredict", methods = ['POST', 'GET'])
+def malariapredictPage():
+    if request.method == 'POST':
+        try:
+            if 'image' in request.files:
+                img = Image.open(request.files['image'])
+                img = img.resize((36,36))
+                img = np.asarray(img)
+                img = img.reshape((1,36,36,3))
+                img = img.astype(np.float64)
+                model = load_model("models/malaria.h5")
+                pred = np.argmax(model.predict(img)[0])
+        except:
+            message = "Please upload an Image"
+            return render_template('malaria.html', message = message)
+    return render_template('malaria_predict.html', pred = pred)
+
+@app.route("/pneumoniapredict", methods = ['POST', 'GET'])
+def pneumoniapredictPage():
+    if request.method == 'POST':
+        try:
+            if 'image' in request.files:
+                img = Image.open(request.files['image']).convert('L')
+                img = img.resize((36,36))
+                img = np.asarray(img)
+                img = img.reshape((1,36,36,1))
+                img = img / 255.0
+                model = load_model("models/pneumonia.h5")
+                pred = np.argmax(model.predict(img)[0])
+        except:
+            message = "Please upload an Image"
+            return render_template('pneumonia.html', message = message)
+    return render_template('pneumonia_predict.html', pred = pred)
+
+if __name__ == '__main__':
+	app.run(debug = True)
